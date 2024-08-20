186d00b323e52eb1473326423c22008c1ed00be7
Cheng Li @ SenseTime <chengli.thu@gmail.com>
2024-08-20 08:33:14 +0800

    第三课增加点餐
    喵

--- pygame_codes/2_three_body.py
+++ pygame_codes/2_three_body.py
@@ -1,72 +1,133 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# 基本常数
-G = 6.67430e-11  # 引力常数, m^3 kg^-1 s^-2
+# 常量定义
+G = 1  # 简化引力常数
+M = 1  # 三个天体的质量相等
 
-# 行星属性：质量 (m), 位置 (x, y), 速度 (vx, vy)
-bodies = [
-    {'m': 5.972e24, 'x': 0, 'y': 0, 'vx': 0, 'vy': 0},   # 地球
-    {'m': 7.348e22, 'x': 3.844e8, 'y': 0, 'vx': 0, 'vy': 2.9783e4},  # 月球
-    {'m': 1.989e30, 'x': 1.496e11, 'y': 0, 'vx': 0, 'vy': 3.0287e4},  # 太阳
-]
+# 初始条件
+r1 = np.array([0.5, 0.5])  # 天体1的位置
+r2 = np.array([-0.5, -0.5])  # 天体2的位置
+r3 = np.array([-0.5, 0.5])  # 天体3的位置
 
-# 时间步长
-dt = 86400  # 一天
+v1 = np.array([0, 0.5])  # 天体1的速度
+v2 = np.array([0, -0.5])  # 天体2的速度
+v3 = np.array([0.5, 0])  # 天体3的速度
 
-# 模拟时间
-t_max = 30 * dt  # 模拟30天
+def force(r1, r2):
+    """计算两个天体之间的引力"""
+    r = r2 - r1
+    distance = np.linalg.norm(r)
+    if distance == 0:
+        return np.zeros(2)
+    return G * M * M * r / distance**3
 
-# 初始化轨迹列表
-trajectories = {i: {'x': [body['x']], 'y': [body['y']]} for i, body in enumerate(bodies)}
+def derivatives(r1, r2, r3, v1, v2, v3):
+    """计算位置和速度的导数"""
+    dr1dt = v1
+    dr2dt = v2
+    dr3dt = v3
+    
+    dv1dt = force(r1, r2) + force(r1, r3)
+    dv2dt = force(r2, r1) + force(r2, r3)
+    dv3dt = force(r3, r1) + force(r3, r2)
+    
+    return dr1dt, dr2dt, dr3dt, dv1dt, dv2dt, dv3dt
 
-# Euler方法更新位置和速度
-def update_bodies(bodies, dt):
-    for i, body_i in enumerate(bodies):
-        # 计算引力
-        for j, body_j in enumerate(bodies):
-            if i != j:
-                # 两物体之间的距离
-                dx = body_j['x'] - body_i['x']
-                dy = body_j['y'] - body_i['y']
-                r = np.sqrt(dx**2 + dy**2)
-                
-                # 引力加速度
-                f = G * body_i['m'] * body_j['m'] / r**2
-                ax = f * dx / r
-                ay = f * dy / r
-                
-                # 更新速度和位置
-                body_i['vx'] += ax * dt
-                body_i['vy'] += ay * dt
-                body_i['x'] += body_i['vx'] * dt
-                body_i['y'] += body_i['vy'] * dt
+def rk4_step(r1, r2, r3, v1, v2, v3, dt):
+    """四阶龙格库塔方法"""
+    dr1dt1, dr2dt1, dr3dt1, dv1dt1, dv2dt1, dv3dt1 = derivatives(r1, r2, r3, v1, v2, v3)
+    
+    r1_2 = r1 + dr1dt1 * dt / 2
+    r2_2 = r2 + dr2dt1 * dt / 2
+    r3_2 = r3 + dr3dt1 * dt / 2
+    v1_2 = v1 + dv1dt1 * dt / 2
+    v2_2 = v2 + dv2dt1 * dt / 2
+    v3_2 = v3 + dv3dt1 * dt / 2
+    
+    dr1dt2, dr2dt2, dr3dt2, dv1dt2, dv2dt2, dv3dt2 = derivatives(r1_2, r2_2, r3_2, v1_2, v2_2, v3_2)
+    
+    r1_3 = r1 + dr1dt2 * dt / 2
+    r2_3 = r2 + dr2dt2 * dt / 2
+    r3_3 = r3 + dr3dt2 * dt / 2
+    v1_3 = v1 + dv1dt2 * dt / 2
+    v2_3 = v2 + dv2dt2 * dt / 2
+    v3_3 = v3 + dv3dt2 * dt / 2
+    
+    dr1dt3, dr2dt3, dr3dt3, dv1dt3, dv2dt3, dv3dt3 = derivatives(r1_3, r2_3, r3_3, v1_3, v2_3, v3_3)
+    
+    r1_4 = r1 + dr1dt3 * dt
+    r2_4 = r2 + dr2dt3 * dt
+    r3_4 = r3 + dr3dt3 * dt
+    v1_4 = v1 + dv1dt3 * dt
+    v2_4 = v2 + dv2dt3 * dt
+    v3_4 = v3 + dv3dt3 * dt
+    
+    dr1dt4, dr2dt4, dr3dt4, dv1dt4, dv2dt4, dv3dt4 = derivatives(r1_4, r2_4, r3_4, v1_4, v2_4, v3_4)
+    
+    r1_new = r1 + (dr1dt1 + 2*dr1dt2 + 2*dr1dt3 + dr1dt4) * dt / 6
+    r2_new = r2 + (dr2dt1 + 2*dr2dt2 + 2*dr2dt3 + dr2dt4) * dt / 6
+    r3_new = r3 + (dr3dt1 + 2*dr3dt2 + 2*dr3dt3 + dr3dt4) * dt / 6
+    
+    v1_new = v1 + (dv1dt1 + 2*dv1dt2 + 2*dv1dt3 + dv1dt4) * dt / 6
+    v2_new = v2 + (dv2dt1 + 2*dv2dt2 + 2*dv2dt3 + dv2dt4) * dt / 6
+    v3_new = v3 + (dv3dt1 + 2*dv3dt2 + 2*dv3dt3 + dv3dt4) * dt / 6
+    
+    return r1_new, r2_new, r3_new, v1_new, v2_new, v3_new
 
-                # 记录轨迹
-                trajectories[i]['x'].append(body_i['x'])
-                trajectories[i]['y'].append(body_i['y'])
+# 模拟参数
+dt = 0.01  # 时间步长
+steps = 1000  # 时间步数
 
-# 进行模拟
-time = 0
-while time < t_max:
-    update_bodies(bodies, dt)
-    time += dt
+# 初始化位置和速度的存储
+r1_list, r2_list, r3_list = [], [], []
 
-# 可视化
-def animate(i):
-    plt.clf()
-    for body in bodies:
-        plt.scatter(body['x'], body['y'], color='blue')
-    for i, trajectory in trajectories.items():
-        plt.plot(trajectory['x'][:i+1], trajectory['y'][:i+1], color='green')
-    plt.xlim(min(trajectory['x']) - 1e8, max(trajectory['x']) + 1e8)
-    plt.ylim(min(trajectory['y']) - 1e8, max(trajectory['y']) + 1e8)
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.title('三体运动模拟')
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
+for _ in range(steps):
+    r1, r2, r3, v1, v2, v3 = rk4_step(r1, r2, r3, v1, v2, v3, dt)
+    r1_list.append(r1)
+    r2_list.append(r2)
+    r3_list.append(r3)
 
-ani = FuncAnimation(plt.gcf(), animate, frames=len(trajectories[0]['x']) - 1, interval=50)
+# 转换为numpy数组以便绘图
+r1_list = np.array(r1_list)
+r2_list = np.array(r2_list)
+r3_list = np.array(r3_list)
+
+# 创建动画
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Three-Body Problem Simulation')
+ax.grid(True)
+
+body1, = ax.plot([], [], 'ro')
+body2, = ax.plot([], [], 'go')
+body3, = ax.plot([], [], 'bo')
+trail1, = ax.plot([], [], 'r-', lw=0.5)
+trail2, = ax.plot([], [], 'g-', lw=0.5)
+trail3, = ax.plot([], [], 'b-', lw=0.5)
+
+def init():
+    body1.set_data([], [])
+    body2.set_data([], [])
+    body3.set_data([], [])
+    trail1.set_data([], [])
+    trail2.set_data([], [])
+    trail3.set_data([], [])
+    return body1, body2, body3, trail1, trail2, trail3
+
+def update(frame):
+    body1.set_data(r1_list[frame, 0], r1_list[frame, 1])
+    body2.set_data(r2_list[frame, 0], r2_list[frame, 1])
+    body3.set_data(r3_list[frame, 0], r3_list[frame, 1])
+    trail1.set_data(r1_list[:frame, 0], r1_list[:frame, 1])
+    trail2.set_data(r2_list[:frame, 0], r2_list[:frame, 1])
+    trail3.set_data(r3_list[:frame, 0], r3_list[:frame, 1])
+    return body1, body2, body3, trail1, trail2, trail3
+
+ani = FuncAnimation(fig, update, frames=range(steps), init_func=init, blit=True, interval=20)
+
 plt.show()
